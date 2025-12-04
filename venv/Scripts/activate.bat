a4eef836bedf1b76287313e0650bde007a67293b
Kyle678 <73669189+Kyle678@users.noreply.github.com>
2025-12-04 10:52:59 -0600

    ignore venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Kyle\Documents\GitHub\Gridsafe-official\venv
+set VIRTUAL_ENV=C:\Users\ktmal\GitHub\Gridsafe-official\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
